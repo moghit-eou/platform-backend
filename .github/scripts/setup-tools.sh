5c1e00999dffd3d54675fed234476b4fedcf882f
moghit-eou <moghit.elouaad1@gmail.com>
2026-06-17 14:31:19 +0100

    refactor(ci): move maven installation to setup script

--- .github/scripts/setup-tools.sh
+++ .github/scripts/setup-tools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+# Installing Maven for SCA dependency resolution
+apt-get update -q
+apt-get install -y maven
+
 # Install Trivy
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.71.1
