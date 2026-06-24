f49112424006750ad7fe3783a980414674e78ddb
moghit-eou <moghit.elouaad1@gmail.com>
2026-06-24 13:16:48 +0100

    fix: add nvd api delay and retry configuration to reduce 503 errors:

--- .github/scripts/run_sca.py
+++ .github/scripts/run_sca.py
@@ -39,6 +39,8 @@ def run_dependency_check():
         "-DfailBuildOnCVSS=7",
         "-DoutputDirectory=.",
         "-DnvdMaxRetryCount=10",
+        "-Dnvd.api.delay=1500",
+        "-Dnvd.api.max.retry.count=20",
        # "-DautoUpdate=false",
     ]
 
