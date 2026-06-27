f54d72b720435819bc0b0bc25eadb4b3eb74c378
moghit-eou <moghit.elouaad1@gmail.com>
2026-06-27 12:06:46 +0100

    fix typo

--- .github/scripts/setup-tools.sh
+++ .github/scripts/setup-tools.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install Trivy
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.71.1a
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.71.1
 
 # Install OSV Scanner
 curl -L https://github.com/google/osv-scanner/releases/download/v2.4.0/osv-scanner_linux_amd64 -o /usr/local/bin/osv-scanner
