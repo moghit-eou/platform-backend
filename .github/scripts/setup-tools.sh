4349d6b2a3355b4554d0b48ad41cd3dc52e8e017
moghit-eou <moghit.elouaad1@gmail.com>
2026-07-22 23:35:48 +0100

    Semgrep installation Fixes

--- .github/scripts/setup-tools.sh
+++ .github/scripts/setup-tools.sh
@@ -20,7 +20,7 @@ OSV_SCANNER_VERSION="${OSV_SCANNER_VERSION:-v2.4.0}"
 OSV_SCANNER_SHA256="${OSV_SCANNER_SHA256:-15314940c10d26af9c6649f150b8a47c1262e8fc7e17b1d1029b0e479e8ed8a0}"
 
 # renovate: datasource=github-release-attachments depName=semgrep/semgrep
-SEMGREP_VERSION="${SEMGREP_VERSION:-v1.167.0}"
+SEMGREP_VERSION="${SEMGREP_VERSION:-v1.170.0}"
 SEMGREP_SHA256="${SEMGREP_SHA256:-bd614accba811b407ae5c9ec6f1eecd3bdc29911}"
 
 # renovate: datasource=npm depName=@cyclonedx/cyclonedx-npm
@@ -57,16 +57,16 @@ sudo install -m 0755 "${TMP_DIR}/osv-scanner" /usr/local/bin/osv-scanner
 osv-scanner --version
 echo "OSV Scanner installed OK"
 
+# renovate: datasource=github-tags depName=semgrep/semgrep
 echo "[setup-tools] Installing Semgrep ${SEMGREP_VERSION}"
-SEMGREP_TARBALL="semgrep-${SEMGREP_VERSION}-ubuntu-16.04.tgz"
+SEMGREP_TARBALL="semgrep-${SEMGREP_VERSION}.tar.gz"
 download_and_verify \
-  "https://github.com/semgrep/semgrep/releases/download/${SEMGREP_VERSION}/${SEMGREP_TARBALL}" \
+  "https://github.com/semgrep/semgrep/archive/refs/tags/${SEMGREP_VERSION}.tar.gz" \
   "${TMP_DIR}/${SEMGREP_TARBALL}" \
   "${SEMGREP_SHA256}"
-sudo tar -xzf "${TMP_DIR}/${SEMGREP_TARBALL}" -C /usr/local/bin --strip-components=1 semgrep-files/semgrep
+sudo tar -xzf "${TMP_DIR}/${SEMGREP_TARBALL}" -C /usr/local/bin
 semgrep --version
 echo "Semgrep installed OK"
-
 
 # Generate SBOM based on project type
 PROJECT_TYPE="${1:-none}"   # maven | npm | none
